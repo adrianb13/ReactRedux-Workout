3f15fe76aead6fcdbb60cc7fc7e7e966d236a831
Adrian Brinoes <Coding4Adrian@yahoo.com>
2019-12-27 11:16:33 -0800

    Updates

---
+++ precache-manifest.23b8fc862e902d375395f00e8df9f97a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5786e0fa3ea75e994749d37873c2387f",
+    "url": "/ReactRedux-Workout/index.html"
+  },
+  {
+    "revision": "a41ce53b0b5c5d97c012",
+    "url": "/ReactRedux-Workout/static/css/main.a28b17db.chunk.css"
+  },
+  {
+    "revision": "b06faef26c4deee8a76f",
+    "url": "/ReactRedux-Workout/static/js/2.af3d9a3a.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/ReactRedux-Workout/static/js/2.af3d9a3a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a41ce53b0b5c5d97c012",
+    "url": "/ReactRedux-Workout/static/js/main.1d54c642.chunk.js"
+  },
+  {
+    "revision": "eef189297cb227bb7acc",
+    "url": "/ReactRedux-Workout/static/js/runtime-main.578d104a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ReactRedux-Workout/precache-manifest.23b8fc862e902d375395f00e8df9f97a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactRedux-Workout/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
